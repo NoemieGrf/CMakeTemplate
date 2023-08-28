f01f5bee5c45cb0aadbe4d12953fcf1eb8c5978e
NoemieGrf <epigrf@gmail.com>
2023-08-28 19:29:46 +0800

    windows resource file

---
+++ Template07_WindowsResource/src/main.cpp
@@ -0,0 +1,14 @@
+
+#include <Windows.h>
+
+
+int WINAPI WinMain(HINSTANCE hInstance,
+HINSTANCE hPrevInstance,
+        PSTR lpCmdLine,
+int nCmdShow)
+{
+
+MessageBox(nullptr,"Hello","Caption",MB_OK);
+
+return 0;
+}
