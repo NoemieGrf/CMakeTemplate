2bfae34cdea47f87ef3f528ae7b38ed9da2a845d
NoemieGrf <epigrf@gmail.com>
2023-08-29 10:24:15 +0800

    code tidy

--- Template08_FindPackage/src/main.cpp
+++ Template08_FindPackage/src/main.cpp
@@ -1,46 +1,55 @@
 #include <iostream>
 #include <Windows.h>
 #include <GL/GL.h>
-#include <GL/GLU.h>
 
-// 窗口过程函数
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    switch (message) {
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
         case WM_DESTROY:
             PostQuitMessage(0);
             break;
         default:
             return DefWindowProc(hWnd, message, wParam, lParam);
     }
+
     return 0;
 }
 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    // 注册窗口类
-    WNDCLASS wc = { 0 };
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    // register window class
+    WNDCLASS wc = {0};
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
-    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wc.lpszMenuName = NULL;
+    wc.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
+    wc.lpszMenuName = nullptr;
     wc.lpszClassName = "SimpleOpenGLExample";
 
-    if (!RegisterClass(&wc)) {
-        MessageBox(NULL, "Failed to register the window class", "Error", MB_ICONERROR | MB_OK);
+    if (!RegisterClass(&wc))
+    {
+        MessageBox(nullptr, "Failed to register the window class", "Error", MB_ICONERROR | MB_OK);
         return -1;
     }
 
-    HWND hWnd = CreateWindow("SimpleOpenGLExample", "Simple OpenGL Example", WS_OVERLAPPEDWINDOW,
-                             CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, nullptr, nullptr, hInstance, nullptr);
+    HWND hWnd = CreateWindow("SimpleOpenGLExample",
+                             "Simple OpenGL Example",
+                             WS_OVERLAPPEDWINDOW,
+                             CW_USEDEFAULT,
+                             CW_USEDEFAULT,
+                             800, 600, nullptr, nullptr, hInstance, nullptr);
 
-    if (!hWnd) {
+    if (!hWnd)
+    {
         MessageBox(NULL, "Failed to create the window", "Error", MB_ICONERROR | MB_OK);
         return -1;
     }
 
-    // 初始化 OpenGL 上下文
-    PIXELFORMATDESCRIPTOR pfd = {
+    // init openGL context
+    PIXELFORMATDESCRIPTOR pfd =
+            {
             sizeof(PIXELFORMATDESCRIPTOR),
             1,
             PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    // Flags
@@ -76,16 +85,18 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     HGLRC hRC = wglCreateContext(hDC);
     wglMakeCurrent(hDC, hRC);
 
-    // 设置 OpenGL 视口
+    // set openGL viewport
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glViewport(0, 0, 800, 600);
 
-    // 渲染循环
+    // render loop
     ShowWindow(hWnd, nCmdShow);
     UpdateWindow(hWnd);
 
-    MSG msg = { 0 };
-    while (GetMessage(&msg, nullptr, 0, 0)) {
+    MSG msg = { nullptr };
+
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
         if (msg.message == WM_QUIT)
             break;
 
@@ -108,7 +119,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     }
 
     // 释放资源
-    wglMakeCurrent(NULL, NULL);
+    wglMakeCurrent(nullptr, nullptr);
     wglDeleteContext(hRC);
     ReleaseDC(hWnd, hDC);
     DestroyWindow(hWnd);
